4680f036ed447cca2864807090edb84b8887193a
pierre.bockner <misc.pierre@posteo.de>
2017-05-31 00:43:12 +0200

    print test string from the match beginning

--- test_find_substring.bat
+++ test_find_substring.bat
@@ -4,41 +4,48 @@ set "sub=14gb a"
 set "str=Hallo 123141 14gb aaasd as 9999aad"
 set pos=pos
 call find_substring %pos% "%str%" "%sub%"
-echo Ergebnis "%pos%"
+call set str=%%str:~%pos%%%
+echo Ergebnis "%pos%": %str%
 
 
 set "sub=1abs"
 set "str=Hallo 123141 14gb aaasd as 9999aad"
 set pos=pos
 call find_substring %pos% "%str%" "%sub%"
-echo Ergebnis "%pos%"
+call set str=%%str:~%pos%%%
+echo Ergebnis "%pos%": %str%
 
 set "sub=Hallo"
 set "str=Hallo 123141 14gb aaasd as 9999aad"
 set pos=pos
 call find_substring %pos% "%str%" "%sub%"
-echo Ergebnis "%pos%"
+call set str=%%str:~%pos%%%
+echo Ergebnis "%pos%": %str%
 
 set "sub=ad"
 set "str=Hallo 123141 14gb aaasd as 9999aad"
 set pos=pos
 call find_substring %pos% "%str%" "%sub%"
-echo Ergebnis "%pos%"
+call set str=%%str:~%pos%%%
+echo Ergebnis "%pos%": %str%
 
 set "sub=l"
 set "str=Hallo 123141 14gb aaasd as 9999aad"
 set pos=pos
 call find_substring %pos% "%str%" "%sub%"
-echo Ergebnis "%pos%"
+call set str=%%str:~%pos%%%
+echo Ergebnis "%pos%": %str%
 
 set "sub=Hallo 123141 14gb aaasd as 9999aad"
 set "str=Hallo 123141 14gb aaasd as 9999aad"
 set pos=pos
 call find_substring %pos% "%str%" "%sub%"
-echo Ergebnis "%pos%"
+call set str=%%str:~%pos%%%
+echo Ergebnis "%pos%": %str%
 
 set "sub=Hallo 123141 14gb aaasd as 9999aad sdg dgdf d"
 set "str=Hallo 123141 14gb aaasd as 9999aad"
 set pos=pos
 call find_substring %pos% "%str%" "%sub%"
-echo Ergebnis "%pos%"
+call set str=%%str:~%pos%%%
+echo Ergebnis "%pos%": %str%
